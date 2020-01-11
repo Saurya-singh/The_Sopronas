8727b31db33b8f02925e4525f04524ad76656f03
biipiinho <58837427+biipiinho@users.noreply.github.com>
2020-01-11 21:43:42 +0545

    tried models

--- The_Sopranos/Matchmaking/models.py
+++ The_Sopranos/Matchmaking/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Status (models.Model):
+    fullname = models.CharField(max_length=40)
+    status = models.CharField(max_length=200)
+    
