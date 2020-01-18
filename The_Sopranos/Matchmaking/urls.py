7e1d0017a94c1dba1e0c28e5c22d5da5e848a0ac
biipiinho <58837427+biipiinho@users.noreply.github.com>
2020-01-19 00:32:47 +0545

    CRUD

--- The_Sopranos/Matchmaking/urls.py
+++ The_Sopranos/Matchmaking/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('status_list',views.status_list,name='status_list'),
     path('home',views.home, name='home'),
     path('notification',views.notification, name='nofification'),
     path('about',views.about, name='about'),
     path('contact',views.contact, name='contact'),
     path('search',views.search, name='search'),
+    path('poststatus',views.status_form, name='status_insert'),
+    path('<int:id>/',views.status_form,name='status_update'),
+    path('list',views.status_list, name="status_list"),
+    path('delete/<int:id>/',views.status_delete,name='status_delete'),
 ]
--- The_Sopranos/Matchmaking/views.py
+++ The_Sopranos/Matchmaking/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import statusform
+from .models import Status
 
 # Create your views here.
 
@@ -14,10 +16,31 @@ def search(request):
     return render(request, 'search.html')
 
 def status_list(request):
-    return render(request, "./status_list.html")
+    context = {'status_list': Status.objects.all()}
+    return render(request, "./status_list.html", context)
 
-def status_form(request):
-    return render(request, "./home.html")
 
-def status_delete(request):
-    return 
+             
+def status_form(request, id=0):
+    if request.method == "GET":
+        if id == 0:
+            form = statusform()
+        else:
+            status = Status.objects.get(pk=id)
+            form = statusform(instance=status)
+        return render(request, "./status_form.html", {'form': form})
+    else:
+        if id == 0:
+            form = statusform(request.POST)
+        else:
+            status = Status.objects.get(pk=id)
+            form = statusform(request.POST,instance= status)
+        if form.is_valid():
+            form.save()
+        return redirect('/list')
+
+def status_delete(request,id):
+    status = Status.objects.get(pk=id)
+    status.delete()
+    return redirect('/list')
+    
