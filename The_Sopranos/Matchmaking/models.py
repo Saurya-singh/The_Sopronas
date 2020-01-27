8a51f2f79227a416a6ea5f6ec69889f5e273410e
nileshgupta3133 <nileshcreetis@gmail.com>
2020-01-27 16:55:53 +0545

    2 testcases complete

--- The_Sopranos/Matchmaking/models.py
+++ The_Sopranos/Matchmaking/models.py
@@ -7,7 +7,14 @@ class Status (models.Model):
     yourpost = models.TextField()
     posteddate = models.DateTimeField(auto_now_add=False, default=tz.now)
 
-def __str__(self):
-    return str(self.id) + " " + self.fullname + " " + self.yourpost
+
+    def __str__(self):
+        return str(self.id) + " " + self.fullname + " " + self.yourpost
 
     
+    def valid(self):
+        return self.fullname != None 
+
+
+    def length(self):
+        return len(self.yourpost)<20
--- The_Sopranos/Matchmaking/tests.py
+++ The_Sopranos/Matchmaking/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from Matchmaking.models import Status
 # Create your tests here.
+
+class Setup_Test(TestCase):
+    def setUp(self):
+        statobj = Status.objects.create(fullname = "Ram", yourpost = "my post less than20")
+
+    def test_valid(self):
+        statobj = Status.objects.get(fullname = "Ram")
+        value = statobj.valid()
+        self.assertTrue(value,True)
+
+
+    def test_valid_postlength(self):
+        statobj = Status.objects.get(fullname = "Ram")
+        value  = statobj.length()
+        self.assertTrue(value,True)
+
+
+
+
